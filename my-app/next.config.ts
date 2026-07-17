a14046bc2ebbe818f0a4f536d35127efad5dfb60
rizkyrachma <rizkyrachma292@gmail.com>
2026-07-17 09:29:02 +0700

    Allow external images from placehold.co

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
