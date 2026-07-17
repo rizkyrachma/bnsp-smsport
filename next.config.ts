73aa3247cbd8f4f4ddc8b42f741c656a12bfc021
rizkyrachma <rizkyrachma292@gmail.com>
2026-07-17 19:04:10 +0700

    fix: change dev script flag to --webpack

--- next.config.ts
+++ next.config.ts
@@ -11,20 +11,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  turbopack: {},
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        ...config.watchOptions,
-        ignored: [
-          "**/node_modules/**",
-          "**/.next/**",
-          "**/my-app/**",
-        ],
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
